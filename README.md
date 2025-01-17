3d9285e2099961ab5b40a81849f408fc5ac43968
hdbugsbunny <harshitpal26697@gmail.com>
2025-01-17 14:40:22 +0530

    First Project Setup For Writing Test Cases

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # react-testing-jest
+
+Here are the projects that will be tested for learning
---
+++ first-testing-project/src/App.js
@@ -0,0 +1,10 @@
+import ProductList from "./components/ProductList";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="container mx-auto">
+      <ProductList />
+    </div>
+  );
+}
