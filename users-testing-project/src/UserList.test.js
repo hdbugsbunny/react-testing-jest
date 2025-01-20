7786b28d9482207ec438aab7e295d5f61853dd95
hdbugsbunny <harshitpal26697@gmail.com>
2025-01-20 11:40:46 +0530

    Avoiding BeforeEach

--- users-testing-project/src/UserList.test.js
+++ users-testing-project/src/UserList.test.js
@@ -1,7 +1,7 @@
 import { render, screen, within } from "@testing-library/react";
 import UserList from "./UserList";
 
-test("render one row per user", () => {
+function renderComponent() {
   // Mock Data
   const users = [
     { userName: "John Doe", userEmail: "john.doe@example.com" },
@@ -9,31 +9,36 @@ test("render one row per user", () => {
   ];
 
   // Render The Component
-  // Fallback - #2: For Query Selection
   const { container } = render(<UserList users={users} />);
 
-  // Find All The Rows in The Table
-  // Fallback - #1: By Data-TestId
-  const rows1 = within(screen.getByTestId("users")).getAllByRole("row");
+  return { users, container };
+}
 
-  // Fallback - #2: By Query Selection
-  // eslint-disable-next-line
-  const rows2 = container.querySelectorAll("tbody tr");
+test("render one row per user FallBack #1: By Data-TestId", () => {
+  // Getting Users From Render Component
+  const { users } = renderComponent();
+
+  // Find All The Rows in The Table
+  const rows = within(screen.getByTestId("users")).getAllByRole("row");
 
   // Assertion: Correct Number of Rows in The Table
-  expect(rows1).toHaveLength(users.length);
-  expect(rows2).toHaveLength(users.length);
+  expect(rows).toHaveLength(users.length);
+});
+
+test("render one row per user Fallback #2: For Query Selection", () => {
+  // Getting Users & Container From Render Component
+  const { users, container } = renderComponent();
+
+  // Find All The Rows in The Table
+  // eslint-disable-next-line
+  const rows = container.querySelectorAll("tbody tr");
+
+  // Assertion: Correct Number of Rows in The Table
+  expect(rows).toHaveLength(users.length);
 });
 
 test("render the email and name of each user", () => {
-  // Mock Data
-  const users = [
-    { userName: "John Doe", userEmail: "john.doe@example.com" },
-    { userName: "Harshit", userEmail: "harshit@example.com" },
-  ];
-
-  // Render The Component
-  render(<UserList users={users} />);
+  const { users } = renderComponent();
 
   // Find All The Rows in The Table
   const rows = within(screen.getByTestId("users")).getAllByRole("row");
